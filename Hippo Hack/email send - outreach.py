4f092f2d86ac179d71ac99a720b2fd419f7d9cd1
Michael Zhang <mzhang0213@gmail.com>
2023-05-05 15:44:36 -0400

    fixed bugs with email bots

--- Hippo Hack/email send - outreach.py
+++ Hippo Hack/email send - outreach.py
@@ -143,4 +143,4 @@ print("finished sending :)")
 
 #confirmation
 body="SENT EMAIL ON '" + sendDate.strftime("%c") + "'\nheres the last (sample) message:\n\n--------------------------------------------------\n\n"+body
-send_email("confirmation sent "+rawData.length+"emails | "+subject, body, sender, "24zhangm@abschools.org", password, names)
+send_email("confirmation sent "+len(rawData)+"emails | "+subject, body, sender, "24zhangm@abschools.org", password, names)
